89a09d7949e359b41ef69e088ab4ee3e5aeeaef1
Anjanikumar Guruvali <108091230+Anjanikumar-G@users.noreply.github.com>
2024-06-05 19:30:46 +0530

    Update weatherController.js

--- src/controllers/weatherController.js
+++ src/controllers/weatherController.js
@@ -25,53 +25,21 @@ async function saveDataToDatabase(data) {
   });
 }
 
-
-/*
-  Instructions for students:
-  Implement the function to retrieve the weather data for a city by its zipcode.
-
-  Function:
-    getWeatherDataByZipCode(zipCode)
-
-  Input:
-    - zipCode (string): The zipcode of the city for which weather data needs to be retrieved.
-
-  Output:
-    - If the city zipcode is found in the database, return the weather data object for the specified city.
-    - If the city zipcode is not found in the database, return new Error('ZipCode not found').
-
-  Tips:
-    - Use the getDataFromDatabase() function to retrieve the data from the database.
-    - Handle the data and filter the weather data based on the provided zipcode.
-    - Return the filtered weather data or null based on the search result.
-*/
-
-
-// Level 3: Get City Weather Data by ZipCode
-async function getWeatherDataByZipCode(zipCode) {
-   // TODO: Implement this function
+// Level 1: Get City Weather Data by Name
+async function getWeatherDataByName(cityName) {
+   console.log("city in controller", cityName); //here we got the city name
    try{
-      console.log("inside getdata");
-      const weatherData= await getDataFromDatabase();
-      let product= weatherData.find(item=> item.zipCode==zipCode);
-      return new Promise((resolve,reject)=>{
-          if(product)
-          {
-             
-             resolve(product.forecast);
-          }
-          else{
-             reject(new Error(zipCode+" not found"));
-          }
-        })
-    }
-    catch(error){
-        throw new Error("Error in database");
-    }
+    const cityData = await getDataFromDatabase();
+    const weatherData = cityData.find((el)=> el.city.toLowerCase() === cityName.toLowerCase());
+    console.log(weatherData);
+    return weatherData.weather
+   }catch(err){
+    console.log(err);
+    throw new Error("City not found");
+   }
 }
 
 
-
 module.exports = {
-  getWeatherDataByZipCode
+  getWeatherDataByName
 };
